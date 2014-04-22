8dd1e05388e485020c769132c2f705a500b7f655
Rodney Dennis <rdnydnns@gmail.com>
2014-04-21 22:36:52 -0500

    first commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+# If you have OpenSSL installed, we recommend updating
+# the following line to use "https"
+source 'http://rubygems.org'
+
+gem "middleman", "~> 3.3.2"
+gem "middleman-blog", "~> 3.5.2"
+
+# For feed.xml.builder
+gem "builder", "~> 3.0"
+
+# FatOak Customizations
+gem "middleman-livereload", "~> 3.1.0"
+gem "middleman-minify-html"
+gem "middleman-imageoptim", "~> 0.1.4"
+gem "middleman-syntax"
+gem "redcarpet"
+gem "readingtime"
+gem "nokogiri"
---
+++ config.rb
@@ -0,0 +1,116 @@
+###
+# Blog settings
+###
+
+# Time.zone = "UTC"
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "blog"
+
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  blog.sources = "/posts/:year-:month-:day-:title.html"
+  # blog.taglink = "tags/{tag}.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  blog.default_extension = ".md"
+
+  # blog.tag_template = "tag.html"
+  # blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
+end
+
+page "/feed.xml", layout: false
+
+# Use pretty URLs
+activate :directory_indexes
+
+# Reload the browser automatically whenever files change
+activate :livereload
+
+# Use HAML markup syntax
+set :haml, {ugly: true, :format => :html5}
+
+###
+# Compass
+###
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", layout: false
+#
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy pages (http://middlemanapp.com/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'scripts'
+
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+  # Minify asset files
+  activate :minify_css
+  activate :minify_javascript
+  activate :minify_html
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
+
+# Compress images
+activate :imageoptim
+
+# Add syntax highlighter
+activate :syntax, :line_numbers => true
+set :markdown_engine, :redcarpet
+set :markdown, :smartypants => true, :tables => true, :autolink => true, :gh_blockcode => true, :fenced_code_blocks => true
